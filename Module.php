2a152489e456fe5b0aac64cd1ef10d04ff8d6dd8
Marius B <infiniloop@users.noreply.github.com>
2015-09-27 18:35:26 +0300

    Update Module.php
    Added support for multiple database configurations

--- Module.php
+++ Module.php
@@ -1,6 +1,8 @@
 <?php
 namespace infiniloop\modules\backuprestore;
 
+use Yii;
+
 class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'infiniloop\modules\backuprestore\controllers';
@@ -8,8 +10,23 @@ class Module extends \yii\base\Module
     // list of databases as defined in the components objects of the current app
     public $databases = [];
 
+    // folder where all backups will be located, relative to the root folder app
+    public $backupFolder = '_backup';
+
+    // path where the mysql binaries are located, if empty it expects they're available globally
+    public $mySqlBasePath = '';
+
     public function init()
     {
         parent::init();
+
+        if (empty($this->databases)) {
+            $components = \Yii::$app->getComponents();
+            foreach ($components as $key => $component) {
+                if (array_key_exists('class', $component) && $component['class'] === 'yii\db\Connection') {
+                    $this->databases[] = $key;
+                }
+            }
+        }
     }
 }
